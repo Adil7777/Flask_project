dd6c5d38bd383e40986ef087c5d25f368f74fe5e
Adil7777 <ismagambetov,adil2005@gmail.com>
2020-04-01 18:14:19 +0600

    Добавил вторую страницу почти сделал третью

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ def index():
     return render_template('test.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/currency_rate')
+def currency_rate():
+    return render_template('currency_rate')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
