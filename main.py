af8d8cf0ff14939989764a35f191075c922d6b84
Adil7777 <ismagambetov,adil2005@gmail.com>
2020-04-02 15:07:24 +0600

    Сделал четвертую страницу

--- main.py
+++ main.py
@@ -18,5 +18,10 @@ def currency_rate():
     return render_template('currency_rate.html')
 
 
+@app.route('/graphs')
+def graphs():
+    return render_template('graphs.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
