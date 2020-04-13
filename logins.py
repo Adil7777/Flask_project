552afc4b64c6b4dbfeeb3b2c327f1825be9fce8e
Adil7777 <57364623+Adil7777@users.noreply.github.com>
2020-04-13 21:38:03 +0600

    Сохраняет пароли

--- logins.py
+++ logins.py
@@ -1,7 +1,19 @@
+logins = []
 LOGINS = []
+EMAILS = {}
 
-with open("logins.txt") as file_handler:
+with open("login.txt") as file_handler:
     for line in file_handler:
-        LOGINS.append(line.replace('\n', ''))
+        logins.append(line + '\n')
+        line = line.split(' ')
+        LOGINS.append(line[0].replace('\n', ''))
+        EMAILS[line[1].replace('\n', '')] = line[0].replace('\n', '')
+print(EMAILS)
 
-print(LOGINS)
+
+def add_account(name, password, email):
+    logins.append('{}:{} {}\n'.format(name, password, email))
+    LOGINS.append('{}:{}'.format(name, password))
+    EMAILS[email] = ['{}:{}'.format(name, password)]
+    with open("login.txt", "w+") as file1:
+        file1.writelines(logins)
